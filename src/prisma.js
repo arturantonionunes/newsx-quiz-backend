56d3913a330567efefb84a83553c35c4993eda0e
Artur Nunes <arturantoniojj@gmail.com>
2025-06-06 15:41:18 -0300

    Database update

--- src/prisma.js
+++ src/prisma.js
@@ -1,4 +1,14 @@
 const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, // Necessário para Supabase
+      },
+    },
+  },
+})
 
 module.exports = prisma
